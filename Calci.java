591d90d2876893b428f70d17410222132781395b
Subrat0007s <subratmantry2@gmail.com>
2024-05-20 14:26:01 +0530

    added Driver Class

--- Calci.java
+++ Calci.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 interface Calci{
     int add(int a, int b);
 }
 class CalciImpl{
     int add(int x,int y){
         return x+y;
+    }
+}
+class CalciDriver{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Two Numbers: ");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a+" + "+b+" + "+new CalciImpl().add(a, b));
+        sc.close();
     }
 }
